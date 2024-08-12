4aafc7d6d31839b9cfed707c7687b22ca7b99bb6
arryndoestech <178195651+arryndoestech@users.noreply.github.com>
2024-08-12 19:13:15 +0200

    Video 4 run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-qemu-system-aarch64 -m 1024 -cpu cortex-a76 -M virt -serial mon:stdio -nographic -device loader,addr=0x40800000,cpu-num=0,file=my_tut_os.o
+qemu-system-aarch64 -smp 4 -m 1024 -cpu cortex-a76 -M virt -serial mon:stdio -nographic -device loader,addr=0x40800000,cpu-num=0,file=my_tut_os.o
